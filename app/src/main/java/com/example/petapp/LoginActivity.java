58bc4803782088f2c71b09b82f2575de50d75adb
Willian~ <williansilveira100@hotmail.com>
2024-08-22 21:54:43 -0300

    Adicionando pagina Login

--- app/src/main/java/com/example/petapp/LoginActivity.java
+++ app/src/main/java/com/example/petapp/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.petapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,35 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+    }
+
+    public void logar(View view) {
+        // Pegando os compenentes da interface grafica
+        EditText login = findViewById(R.id.editTextLogin);
+        EditText senha = findViewById(R.id.editTextSenha);
+
+        // Pegando o conteudo da caixa de login
+        String conteudoLogin = login.getText().toString();
+        String conteudoSenha = senha.getText().toString();
+
+        if(conteudoLogin.isEmpty()){
+            Toast.makeText(this, "prencher o campo login", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(conteudoSenha.isEmpty()){
+            Toast.makeText(this, "prencher o campo senha", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(conteudoLogin.equals("admin") && conteudoSenha.equals("123")){
+            // Acessar dashboard
+        }else{
+            Toast.makeText(this, "Usuario ou Senha INVALIDO", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
     }
 }
